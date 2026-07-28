1bcad519ca4dff323c6b9b909f5845a27830abd7
Алексей Миронов <Jianko@hotmail.com>
2026-07-28 22:30:27 +0300

    1234

--- 1_FirstNet.CoreApp/Program.cs
+++ 1_FirstNet.CoreApp/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World, блин!");
+// Проверяем, в какой конфигурации запущена программа
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+var env = app.Environment;
+
+
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/about", () => $"Окружение: {env.EnvironmentName},\nНазвание приложения: {env.ApplicationName}");
+
+app.MapGet("/error", () =>
+{
+    throw new Exception("Тестовая ошибка!");
+});
 
 app.Run();
